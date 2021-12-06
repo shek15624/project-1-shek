914a641f61e2264d20dabcbb48c32eea5c8f1451
Shek <shek15624@gmail.com>
2021-12-06 14:12:12 +0530

    project

---
+++ jsfile.js
@@ -0,0 +1,17 @@
+/*
+function fadeimage()
+{
+    $("#img1").fadeToggle(2000);
+}
+function fadeimage()
+{
+    $("#text").fadeToggle(2000);
+}
+*/
+
+$(document).ready(function(){
+    $(".count-n").counterUp({
+        delay: 10,
+        time: 1200
+    });
+});
